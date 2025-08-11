051da9e03d2a182acc1b54d207a658c54b6e7366
Jonathan E. Corriveau <joncorv@gmail.com>
2025-08-10 22:06:21 -0700

    lualine global status update

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,11 @@
 local colors = {
-  blue   = '#80a0ff',
-  cyan   = '#79dac8',
-  black  = '#080808',
-  white  = '#c6c6c6',
-  red    = '#ff5189',
-  violet = '#d183e8',
-  grey   = '#303030',
+  blue = "#80a0ff",
+  cyan = "#79dac8",
+  black = "#080808",
+  white = "#c6c6c6",
+  red = "#ff5189",
+  violet = "#d183e8",
+  grey = "#303030",
 }
 
 local bubbles_theme = {
@@ -26,40 +26,54 @@ local bubbles_theme = {
   },
 }
 
-
-
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-	require('lualine').setup {
-	  options = {
-	    theme = bubbles_theme,
-	    component_separators = '',
-	    section_separators = { left = '', right = '' },
-	  },
-	  sections = {
-	    lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
-	    lualine_b = { 'filename', 'branch' },
-	    lualine_c = {
-	      '%=', --[[ add your center components here in place of this comment ]]
-	    },
-	    lualine_x = {},
-	    lualine_y = { 'filetype', 'progress' },
-	    lualine_z = {
-	      { 'location', separator = { right = '' }, left_padding = 2 },
-	    },
-	  },
-	  inactive_sections = {
-	    lualine_a = { 'filename' },
-	    lualine_b = {},
-	    lualine_c = {},
-	    lualine_x = {},
-	    lualine_y = {},
-	    lualine_z = { 'location' },
-	  },
-	  tabline = {},
-	  extensions = {},
-	}
-    end,
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.g.lualine_laststatus = vim.o.laststatus
+    if vim.fn.argc(-1) > 0 then
+      -- set an empty statusline till lualine loads
+      vim.o.statusline = " "
+    else
+      -- hide the statusline on the starter page
+      vim.o.laststatus = 0
+    end
+  end,
+
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = auto,
+        globalstatus = vim.o.laststatus == 3,
+        disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
+        component_separators = "",
+        section_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+        lualine_b = { "filename", "branch" },
+        lualine_c = {
+          "%=", --[[ add your center components here in place of this comment ]]
+        },
+        lualine_x = {},
+        lualine_y = { "filetype", "progress" },
+        lualine_z = {
+          { "location", separator = { right = "" }, left_padding = 2 },
+        },
+      },
+      inactive_sections = {
+        lualine_a = { "filename" },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { "location" },
+      },
+      tabline = {},
+      extensions = { "neo-tree", "lazy", "fzf" },
+
+      -- globalstatus = true,
+    })
+  end,
 }
