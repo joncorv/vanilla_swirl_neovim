40fb317bbf3e4e2707654e53d9e03730cb2cef77
Jonathan E. Corriveau <joncorv@gmail.com>
2025-08-10 22:12:40 -0700

    rust and lua lsp's are now working extremely nicely

--- lua/lsp/rust.lua
+++ lua/lsp/rust.lua
@@ -1,27 +1,17 @@
--- Set up rust-analyzer using built-in vim.lsp.config
-vim.lsp.config["rust_analyzer"] = {
-    cmd = { "rust-analyzer" },
-    filetypes = { "rust" },
-    root_markers = { "Cargo.toml", "Cargo.lock" },
-    settings = {
-        ["rust-analyzer"] = {
-            checkOnSave = {
-                command = "cargo check",
-            },
-            cargo = {
-                allFeatures = true,
-            },
-        },
+vim.lsp.config("rust_analyzer", {
+  settings = {
+    ["rust-analyzer"] = {
+      diagnostics = {
+        enable = false,
+      },
+      checkOnSave = {
+        command = "cargo clippy",
+      },
+      cargo = {
+        allFeatures = true,
+      },
     },
-}
+  },
+})
 
 vim.lsp.enable("rust_analyzer")
-
---
--- -- Enable the LSP for Rust files
--- vim.api.nvim_create_autocmd('FileType', {
---   pattern = 'rust',
---   callback = function(args)
---     vim.lsp.start({ name = 'rust_analyzer', bufnr = args.buf })
---   end,
--- })
