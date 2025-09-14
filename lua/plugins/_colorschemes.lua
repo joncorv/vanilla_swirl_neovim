06e8294798d9f7e754ac9d5a1702f9a9d85d7a6c
Jonathan E. Corriveau <joncorv@gmail.com>
2025-09-13 21:00:17 -0700

    colorscheme updarted

--- lua/plugins/_colorschemes.lua
+++ lua/plugins/_colorschemes.lua
@@ -5,16 +5,16 @@ return {
     priority = 1000,
     config = function()
       require("eldritch").setup()
-      -- vim.cmd.colorscheme("eldritch")
+      vim.cmd.colorscheme("eldritch")
     end,
   },
 
   {
     "olimorris/onedarkpro.nvim",
-    config = function()
-      require("onedarkpro").setup()
-      vim.cmd("colorscheme onelight")
-    end,
+    -- config = function()
+    --   require("onedarkpro").setup()
+    --   vim.cmd("colorscheme onelight")
+    -- end,
   },
 
   { "scottmckendry/cyberdream.nvim" },
