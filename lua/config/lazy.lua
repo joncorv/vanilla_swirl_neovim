110f920e2e8272d7081fdb146892d72fb0b4e986
Jonathan Corriveau <joncorv@gmail.com>
2026-01-29 22:12:06 -0800

    lazy.nvim no longer auto checks

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -36,7 +36,7 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "eldritch", "kanagawa" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = false },
 })
 
 -- require("config.options")
