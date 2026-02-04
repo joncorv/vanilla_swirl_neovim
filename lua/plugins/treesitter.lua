9572ebd2821ada833388a23974de4573fe0cee74
Jonathan Corriveau <joncorv@gmail.com>
2026-02-03 21:58:30 -0800

    treesitter fixes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,35 +1,50 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  -- event = "VeryLazy",
+  branch = "main",
+  lazy = false,
+  build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "c",
-        "lua",
-        "vim",
-        "vimdoc",
-        "query",
-        "markdown",
-        "markdown_inline",
-        "vue",
-        "typescript",
-        "javascript",
-        "rust",
-        "ron",
-        "css",
-        "python",
-        "html",
-        "json",
-        "jsonc",
-      },
-      sync_install = false,
-      auto_install = true,
-      ignore_install = {},
+    local ts = require("nvim-treesitter")
 
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
+    -- Install parsers
+    ts.install({
+      "c",
+      "lua",
+      "vim",
+      "vimdoc",
+      "query",
+      "markdown",
+      "markdown_inline",
+      "vue",
+      "typescript",
+      "javascript",
+      "rust",
+      "ron",
+      "css",
+      "python",
+      "html",
+      "json",
+      "jsonc",
+      "nix",
+      "bash",
+      "toml",
     })
+
+    -- Enable treesitter highlighting for all filetypes
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function()
+        pcall(vim.treesitter.start)
+      end,
+    })
+
+    -- toggle on and off treesitter when i hit heavy files
+    vim.keymap.set("n", "<leader>ut", function()
+      local buf = vim.api.nvim_get_current_buf()
+      if vim.b[buf].ts_highlight then
+        vim.treesitter.stop(buf)
+      else
+        vim.treesitter.start(buf)
+      end
+    end, { desc = "Toggle Treesitter" })
   end,
 }
