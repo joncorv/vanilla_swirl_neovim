4759b414f7a4062176f7bd68c7fbf222eca2e852
Jonathan E. Corriveau <joncorv@gmail.com>
2026-02-23 09:57:58 -0800

    conform eslint update

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,17 +4,16 @@ return {
   opts = {
     formatters_by_ft = {
       lua = { "stylua" },
-      -- Conform will run multiple formatters sequentially
       python = { "isort", "black" },
-      -- You can customize some of the format options for the filetype (:help conform.format)
       rust = { "rustfmt" },
-      -- Conform will run the first available formatter
-      javascript = { "prettierd", "prettier", stop_after_first = true },
-      typescript = { "prettierd", "prettier", stop_after_first = true },
+      -- Let ESLint handle JS/TS/Vue formatting via LSP fallback
+      javascript = {},
+      typescript = {},
+      vue = {},
     },
     format_on_save = {
       -- These options will be passed to conform.format()
-      timeout_ms = 500,
+      timeout_ms = 2000,
       lsp_format = "fallback",
     },
   },
