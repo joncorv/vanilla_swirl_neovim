16384720d9bd1fa1fd8cdd7fd8494b0d3a0c9a4f
Jonathan E. Corriveau <joncorv@gmail.com>
2026-02-23 22:10:21 -0800

    eslint config setup

---
+++ lua/lsp/eslint.lua
@@ -0,0 +1,31 @@
+vim.lsp.config("eslint", {
+  settings = {
+    workingDirectories = { mode = "auto" },
+    codeAction = {
+      disableRuleComment = {
+        enable = true,
+        location = "separateLine",
+      },
+      showDocumentation = { enable = true },
+    },
+    format = true,
+    nodePath = "",
+    onIgnoredFiles = "off",
+    problems = { shortenToSingleLine = false },
+    -- "onType" gives live diagnostics as you type (e.g. Nuxt rule violations inline)
+    run = "onType",
+    validate = "on",
+  },
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescriptreact",
+    "typescript.tsx",
+    "vue",
+  },
+})
+
+vim.lsp.enable("eslint")
+
