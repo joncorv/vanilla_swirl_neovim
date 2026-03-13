51d5d69557a76e867c7d4285f724a496339ae0e6
Jonathan E. Corriveau <joncorv@gmail.com>
2026-03-12 22:42:50 -0700

    wgsl analyzer added to nvim-lspconfig

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,4 +1,14 @@
 return {
   "neovim/nvim-lspconfig",
-  -- event = "VeryLazy",
+  config = function()
+    -- Filetype detection for WGSL
+    vim.filetype.add({ extension = { wgsl = "wgsl" } })
+
+    -- wgsl_analyzer is not in Mason; install via:
+    -- cargo install --git https://github.com/wgsl-analyzer/wgsl-analyzer wgsl-analyzer
+    if vim.fn.executable("wgsl-analyzer") == 1 then
+      vim.lsp.config("wgsl_analyzer", {})
+      vim.lsp.enable("wgsl_analyzer")
+    end
+  end,
 }
