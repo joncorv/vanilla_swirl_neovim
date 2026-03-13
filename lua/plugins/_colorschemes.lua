3b0c9013963ec0752c12a19a94ca0e9c97faed07
Jonathan E. Corriveau <joncorv@gmail.com>
2026-03-12 22:42:27 -0700

    colorschemes updated

--- lua/plugins/_colorschemes.lua
+++ lua/plugins/_colorschemes.lua
@@ -14,18 +14,33 @@ return {
   { "savq/melange-nvim" },
   {
     "e-ink-colorscheme/e-ink.nvim",
-    priority = 1000,
-    config = function()
-      require("e-ink").setup()
-      vim.cmd.colorscheme("e-ink")
+    -- priority = 1000,
+    -- config = function()
+    --   require("e-ink").setup()
+    -- vim.cmd.colorscheme("e-ink")
 
-      -- choose light mode or dark mode
-      vim.opt.background = "dark"
-      -- vim.opt.background = "light"
-      --
-      -- or do
-      -- :set background=dark
-      -- :set background=light
+    -- choose light mode or dark mode
+    -- vim.opt.background = "dark"
+    -- vim.opt.background = "light"
+    --
+    -- or do
+    -- :set background=dark
+    -- :set background=light
+    --   end,
+  },
+  {
+    "zenbones-theme/zenbones.nvim",
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    dependencies = "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1000,
+    -- you can set set configuration options here
+    config = function()
+      -- vim.g.zenbones_darken_comments = 45
+      vim.cmd.colorscheme("zenbones")
+      -- vim.cmd.colorscheme("tokyobones")
     end,
   },
 
