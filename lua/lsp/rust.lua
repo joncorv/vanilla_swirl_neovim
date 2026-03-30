884fee8ed019548e1333f87dad457bd6a716975d
joncorv <joncorv@gmail.com>
2026-03-30 09:44:33 -0700

    move rustaceanvim plugin into lsp folder

--- lua/lsp/rust.lua
+++ lua/lsp/rust.lua
@@ -1,43 +1,32 @@
-vim.lsp.config("rust_analyzer", {
-  settings = {
-    ["rust-analyzer"] = {
-      diagnostics = {
-        enable = false,
-      },
-      checkOnSave = {
-        command = "cargo check",
-      },
-      cargo = {
-        allFeatures = true,
-      },
-    },
+-- using rustaceanvim for rust LSP!
+
+return {
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^4",
+    ft = { "rust" },
+    init = function()
+      vim.g.rustaceanvim = {
+        server = {
+          settings = {
+            ["rust-analyzer"] = {
+              checkOnSave = {
+                command = "clippy",
+              },
+              cargo = {
+                allFeatures = true,
+              },
+            },
+          },
+        },
+      }
+    end,
   },
-})
-
-vim.lsp.enable("rust_analyzer")
-
--- return {
---   "mrcjkb/rustaceanvim",
---   version = "^4",
---   lazy = false,
---   ft = { "rust" },
---   init = function()
---     vim.g.rustaceanvim = {
---       server = {
---         settings = {
---           ["rust-analyzer"] = {
---             checkOnSave = {
---               command = "clippy",
---             },
---           },
---         },
---       },
---     }
---   end,
--- }, {
---   "Saecki/crates.nvim",
---   ft = { "toml" },
---   config = function()
---     require("crates").setup({})
---   end,
--- }
+  {
+    "Saecki/crates.nvim",
+    ft = { "toml" },
+    config = function()
+      require("crates").setup({})
+    end,
+  },
+}
