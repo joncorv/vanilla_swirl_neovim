87d7bf3c9378bc999f8b696a8da2f208529980e5
joncorv <joncorv@gmail.com>
2026-03-30 17:46:05 -0700

    great QOL updates

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,13 +4,6 @@
 -- vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 -- vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
-vim.keymap.set("n", "<leader>qq", ":qall<CR>", { desc = "Quit Session" })
--- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
--- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
--- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
--- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
--- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
-
 -- Toggle Neotree in current directory and reveal current file
 vim.keymap.set("n", "\\", ":Neotree toggle current reveal_force_cwd<CR>", {
   desc = "Toggle Neotree (current dir, reveal file)",
--- lua/plugins/persistence.lua
+++ lua/plugins/persistence.lua
@@ -2,11 +2,43 @@ return {
   "folke/persistence.nvim",
   event = "BufReadPre",
   opts = {},
-  -- stylua: ignore
   keys = {
-    { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
-    { "<leader>qS", function() require("persistence").select() end,desc = "Select Session" },
-    { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
-    { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+    {
+      "<leader>qs",
+      function()
+        require("persistence").load()
+      end,
+      desc = "Restore Session",
+    },
+    {
+      "<leader>qS",
+      function()
+        require("persistence").select()
+      end,
+      desc = "Select Session",
+    },
+    {
+      "<leader>ql",
+      function()
+        require("persistence").load({ last = true })
+      end,
+      desc = "Restore Last Session",
+    },
+    {
+      "<leader>qd",
+      function()
+        require("persistence").stop()
+      end,
+      desc = "Don't Save Current Session",
+    },
+    {
+      "<leader>qq",
+      function()
+        vim.cmd("Neotree close")
+        require("persistence").save()
+        vim.cmd("qa")
+      end,
+      desc = "Quit Session",
+    },
   },
 }
