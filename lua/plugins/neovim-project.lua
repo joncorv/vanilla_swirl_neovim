e332eccc1e81104da56da186e5da74e17d08f854
Jonathan E. Corriveau <joncorv@gmail.com>
2026-06-15 20:39:57 -0700

    feat: project & persistence -> neovim-project session management

---
+++ lua/plugins/neovim-project.lua
@@ -0,0 +1,69 @@
+return {
+  "coffebar/neovim-project",
+  lazy = false,
+  priority = 100,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "ibhagwan/fzf-lua",
+    "Shatur/neovim-session-manager",
+  },
+  opts = {
+    -- Glob patterns for project discovery (used by :NeovimProjectDiscover).
+    -- Any directory you open Neovim in is also added to history automatically,
+    -- so these only need to cover where you keep clusters of projects.
+    projects = {
+      "~/Documents/*",
+    },
+    picker = {
+      type = "fzf-lua",
+    },
+  },
+  init = function()
+    -- Persist global variables in the session (needed by session-manager).
+    vim.opt.sessionoptions:append("globals")
+  end,
+  keys = {
+    {
+      "<leader>fp",
+      "<cmd>NeovimProjectHistory<CR>",
+      desc = "Find Recent Projects",
+    },
+    {
+      "<leader>fP",
+      "<cmd>NeovimProjectDiscover<CR>",
+      desc = "Discover Projects",
+    },
+    -- Session management (via neovim-session-manager, bundled with coffebar).
+    -- Sessions auto-save on exit and auto-restore on entering a project; these
+    -- are the manual controls, mirroring the old persistence.nvim keymaps.
+    {
+      "<leader>qs",
+      "<cmd>SessionManager load_current_dir_session<CR>",
+      desc = "Restore Session",
+    },
+    {
+      "<leader>qS",
+      "<cmd>NeovimProjectHistory<CR>",
+      desc = "Select Session",
+    },
+    {
+      "<leader>ql",
+      "<cmd>SessionManager load_last_session<CR>",
+      desc = "Restore Last Session",
+    },
+    {
+      "<leader>qd",
+      "<cmd>SessionManager delete_current_dir_session<CR>",
+      desc = "Delete Session",
+    },
+    {
+      "<leader>qq",
+      function()
+        pcall(vim.cmd, "Neotree close")
+        vim.cmd("SessionManager save_current_session")
+        vim.cmd("qa")
+      end,
+      desc = "Quit Session",
+    },
+  },
+}
