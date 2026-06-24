9551095cd93fe5fa732bdc38ecb3912747da0efa
Jonathan E. Corriveau <joncorv@gmail.com>
2026-06-23 23:14:42 -0700

    feat: markdown obsidian support

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,5 +1,9 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-  opts = {},
+  -- dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.icons" },
+  opts = {
+
+    preset = "obsidian",
+  },
 }
